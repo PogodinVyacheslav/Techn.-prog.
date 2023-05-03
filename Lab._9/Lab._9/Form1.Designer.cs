493dce2615413b491c15a02c241bf79cb5ae65ff
PogodinVyacheslav <121228358+PogodinVyacheslav@users.noreply.github.com>
2023-05-04 00:21:14 +0300

    Add files via upload

---
+++ Lab._9/Lab._9/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Lab._9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(27, 29);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(652, 484);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Вар. 55:";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(715, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Точеч.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(884, 269);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(78, 31);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Click += new System.EventHandler(this.numericUpDown1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(884, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сплайн";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(816, 333);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(89, 29);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Cин.";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(921, 333);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(87, 29);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Зел.";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(865, 383);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(102, 29);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "Оран.";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(800, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 39);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Гист.";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(754, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Тол. дан.:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(710, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Залив.:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(754, 444);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Тол. осей:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(890, 442);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(78, 31);
+            this.numericUpDown2.TabIndex = 17;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Click += new System.EventHandler(this.numericUpDown2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(807, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 25);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Тип. диаг.:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 545);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab._9/Lab._9/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab._9
+{
+    public partial class Form1:Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Axis ox=new Axis();
+            ox.Title="Ось Х";
+            chart1.ChartAreas[0].AxisX=ox;
+            Axis oy=new Axis();
+            oy.Title="Ось Y";
+            chart1.ChartAreas[0].AxisY=oy;
+        }
+
+        private void button1_MouseClick(object sender,MouseEventArgs e)
+        {
+            try
+            {
+                int t=(int)numericUpDown1.Value;
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[2].Points.Clear();
+
+                double y;
+                for(int x=1; x<7; x++)
+                {
+                    y=x^3+x^4-x^5;
+                    chart1.Series[0].Points.AddXY(x,y);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Нет фильт.");
+            }
+        }
+
+        private void button2_MouseClick(object sender,MouseEventArgs e)
+        {
+            try
+            {
+                int t=(int)numericUpDown1.Value;
+
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[2].Points.Clear();
+
+                double y;
+                for (int x=1; x<7; x++)
+                {
+                    y=x^3+x^4-x^5;
+                    chart1.Series[1].Points.AddXY(x,y);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Нет фильт.");
+            }
+        }
+
+        private void button3_Click(object sender,EventArgs e)
+        {
+            try
+            {
+                int t=(int)numericUpDown1.Value;
+                chart1.Series[2].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[0].Points.Clear();
+
+                double y;
+                for(int x=1; x<7; x++)
+                {
+                    y=x^3+x^4-x^5;
+                    chart1.Series[2].Points.AddXY(x,y);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Нет фильт.");
+            }
+        }
+
+        private void numericUpDown1_Click(object sender,EventArgs e)
+        {
+            int t=(int)numericUpDown1.Value;
+            chart1.Series[0].BorderWidth=t;
+            chart1.Series[1].BorderWidth=t;
+            chart1.Series[2].BorderWidth=t;
+        }
+
+        private void numericUpDown2_Click(object sender,EventArgs e)
+        {
+            int t=(int)numericUpDown2.Value;
+            chart1.ChartAreas[0].AxisX.LineWidth=t;
+            chart1.ChartAreas[0].AxisY.LineWidth=t;
+        }
+
+        private void checkBox3_CheckedChanged(object sender,EventArgs e)
+        {
+            if(checkBox3.Checked)
+            {
+                checkBox4.Checked=false;
+                checkBox5.Checked=false;
+                chart1.Series[0].Color=Color.Blue;
+                chart1.Series[1].Color=Color.Blue;
+                chart1.Series[2].Color=Color.Blue;
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender,EventArgs e)
+        {
+            if(checkBox4.Checked)
+            {
+                checkBox3.Checked=false;
+                checkBox5.Checked=false;
+                chart1.Series[0].Color=Color.Green;
+                chart1.Series[1].Color=Color.Green;
+                chart1.Series[2].Color=Color.Green;
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender,EventArgs e)
+        {
+            if(checkBox5.Checked)
+            {
+                checkBox4.Checked=false;
+                checkBox3.Checked=false;
+                chart1.Series[0].Color=Color.Orange;
+                chart1.Series[1].Color=Color.Orange;
+                chart1.Series[2].Color=Color.Orange;
+            }
+        }
+    }
+}
